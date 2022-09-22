9491add36f04999bb02dde3ec1d7ba9325f2e107
PlubCPE <chakkapop_s@cmu.ac.th>
2022-09-22 22:10:43 +0700

    finish

--- pages/api/room.js
+++ pages/api/room.js
@@ -2,4 +2,22 @@ import { readDB } from "../../backendLibs/dbLib";
 
 export default function roomRoute(req, res) {
   const rooms = readDB();
+
+  // const newrooms = [];
+  // for (const room of rooms) {
+  //   newrooms.push({
+  //     roomId: room.roomId,
+  //     roomName: room.roomName,
+  //   });
+  // }  //ใช้ได้สองแบบ
+
+  const newrooms = rooms.map((x) => ({
+    roomId: x.roomId,
+    roomName: x.roomName,
+  }));
+
+  return res.json({
+    ok: true,
+    newrooms,
+  });
 }
--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,6 +4,28 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
+
+    const id = req.query.roomId;
+    const roomsIdx = rooms.findIndex((x) => x.roomId === id);
+    if (roomsIdx === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    } else {
+      const messages = [];
+      for (const room of rooms) {
+        if (room.roomId === id) {
+          for (const messages1 of room.messages) {
+            messages.push({
+              messageId: messages1.messageId,
+              text: messages1.text,
+            });
+          }
+        }
+      }
+      return res.json({
+        ok: true,
+        messages,
+      });
+    }
   } else if (req.method === "POST") {
     const rooms = readDB();
 
@@ -12,5 +34,33 @@ export default function roomIdMessageRoute(req, res) {
 
     //create new id
     const newId = uuidv4();
+
+    const roomId = req.query.roomId;
+    const room = rooms.find((x) => x.roomId === roomId);
+    if (room === false) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    } else {
+      if (typeof text !== "string" && text.length <= 0) {
+        return res
+          .status(400)
+          .json({ ok: false, message: "Invalid text input" });
+      }
+      // const message = {
+      //   messageId: `${newId}`,
+      //   text: `${text}`,
+      // };
+      room.messages.push({
+        messageId: newId,
+        text: text,
+      });
+      writeDB(rooms);
+      return res.status(200).json({
+        ok: true,
+        message: {
+          messageId: newId,
+          text: text,
+        },
+      });
+    }
   }
 }
